4e0e30db333742d0726de104a42c8a0defe1ff10
Rémy Trohel <remy.trohel@gmail.com>
2019-06-25 17:59:37 +0200

    quest completed

---
+++ src/main/java/com/wildcodeschool/myJDBCProject/MyJdbcProjectApplication.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.myJDBCProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJdbcProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyJdbcProjectApplication.class, args);
+	}
+
+}
